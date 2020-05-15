e56d2ac85a918061201f46e0a461307ee22065f1
Guilherme Luchesi <gui.olhenrique@gmail.com>
2020-05-14 21:10:36 -0300

    Cria arquivo index que roda script e trata argumentos

---
+++ index.js
@@ -0,0 +1,11 @@
+const { csvParser } = require('./start');
+
+const targetSite = process.argv[2];
+const csvDest = process.argv[3];
+const pagesToScrape = process.argv[4];
+
+if(!targetSite) {
+  console.log('Por favor, informe um site para o scrapping')
+}
+
+csvParser(targetSite, csvDest, pagesToScrape);
